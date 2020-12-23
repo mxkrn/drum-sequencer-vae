7c7cbca0b074fa883c9950f4e2410bc4d1e7aa04
mxkrn <maxkraan1@gmail.com>
2020-12-23 17:57:52 +0100

    Add gcloud config

---
+++ gcloud/client.py
@@ -0,0 +1,7 @@
+instance_types = [
+    "a2-highgpu-1g",
+    ""
+]
+
+region = "us-central1"
+zone = "us-central1-a"
---
+++ gcloud/ssh.sh
@@ -0,0 +1,1 @@
+gcloud beta compute ssh --zone "us-central1-a" "gpu-training-vm" --project "drum-sequencer-vae-training"
