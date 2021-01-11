e55ce7c543f29e597f2caaba723ec7b3b7bc8f4c
mxkrn <maxkraan1@gmail.com>
2021-01-11 10:39:49 +0100

    Hot fix: update NoteDropout; also update default config
    Hot fix

--- dsvae/models/utils.py
+++ dsvae/models/utils.py
@@ -10,6 +10,6 @@ class NoteDropout(nn.Module):
     def forward(self, input: torch.Tensor, p: torch.Tensor) -> torch.Tensor:
         ones = torch.ones(input.shape, dtype=torch.float, device=input.device)
         zeros = torch.zeros(input.shape, dtype=torch.float, device=input.device)
-        mask = torch.rand(input.shape, dtype=torch.float, requires_grad=False)
+        mask = torch.rand(input.shape, dtype=torch.float, device=input.device)
         mask = torch.where(mask <= p.to(input.device), ones, zeros)
         return torch.mul(input, mask)
